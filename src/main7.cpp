da0ce3caa49ef87c7d3dd1dd790870aba5c9f7af
IFedyanin <logowaz.corp@mail.ru>
2020-02-24 13:40:04 +0300

    Lab 3 is ready b61

---
+++ src/main7.cpp
@@ -0,0 +1,62 @@
+﻿#include <stdio.h>
+#include <string.h>
+#define SIZE 128
+
+int sum(int* arr, int N)
+{
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+        sum += arr[i];
+
+    return sum;
+}
+
+int max(int* arr, int N)
+{
+    int max = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+
+    return max;
+}
+
+int main()
+{
+    char buf[SIZE] = { 0 };
+    int count[SIZE] = { 0 };
+
+    printf("Input the line\n");
+    fgets(buf, SIZE, stdin);
+
+    if (buf[strlen(buf) - 1] == '\n')
+        buf[strlen(buf) - 1] = '\0';
+
+    for (int i = 0; buf[i]; i++)
+        count[buf[i]]++;
+
+    int summaAll = sum(count, SIZE);
+    int maximumAll = max(count, SIZE);
+    int summa = sum(count, SIZE); int maximum = max(count, SIZE);
+    float freq = 0;
+
+    while (summa > 0)
+    {
+        for (int i = 0; (i < SIZE) && (summa > 0); i++)
+        {
+            if (count[i] == maximum)
+            {
+                //printf("%c - %d\n", i, count[i]);
+                freq = ((float)count[i] / (float)summaAll);
+                printf("%c - %d - %1.4f\n", i, count[i], freq);
+                count[i] = 0;
+            }
+            summa = sum(count, SIZE);
+            maximum = max(count, SIZE);
+        }
+    }
+    return 0;
+}
